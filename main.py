958d2547ca7bbeea7e77f3a0b830844aa6a5208c
Slava Maksimov <hello@slavamak.com>
2024-02-09 18:15:56 +0300

    refactor: use constants

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ import os
 import csv
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-if not os.getenv("ACCESS_TOKEN"):
-    raise EnvironmentError(
-        "Required environment variable 'ACCESS_TOKEN' is not set.")
-
-output_directory = "assets"
-output_file_name = "subscription-contracts.csv"
-columns = ["id", "address1", "status", "next_charge_scheduled_at"]
+OUTPUT_DIRECTORY = "output"
+OUTPUT_FILE_NAME = "subscription-contracts.csv"
+COLUMNS = ["id", "address1", "status", "next_charge_scheduled_at"]
 
 
 def fetch_data(endpoint):
     access_token = os.getenv("ACCESS_TOKEN")
     api_url = os.getenv("API_URL")
+    if not access_token:
+        raise EnvironmentError(
+            "Required environment variable 'ACCESS_TOKEN' is not set.")
+
     headers = {"X-Recharge-Access-Token": access_token,
                "Accept": "application/json"}
-
     response = requests.get(f"{api_url}{endpoint}", headers=headers)
     if response.status_code != 200:
-        print("Failed to fetch data:", response.status_code)
+        raise ValueError(f"Failed to fetch data: {response.status_code}")
 
     return response.json()
 
@@ -35,18 +33,17 @@ def fetch_subscriptions():
     for subscription in subscriptions:
         address_id = subscription["address_id"]
         address_data = fetch_data(f"/addresses/{address_id}")["address"]
-
         subscription.update(address_data)
         data.append(subscription)
 
     return data
 
 
-def generate_csv(data, dir, filename, column_names):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+def generate_csv(data, directory, filename, column_names):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    file_path = os.path.join(dir, filename)
+    file_path = os.path.join(directory, filename)
     with open(file_path, mode='w', newline='') as file:
         writer = csv.DictWriter(
             file, fieldnames=column_names, extrasaction="ignore")
@@ -57,7 +54,7 @@ def generate_csv(data, dir, filename, column_names):
 
 def main():
     data = fetch_subscriptions()
-    generate_csv(data, output_directory, output_file_name, columns)
+    generate_csv(data, OUTPUT_DIRECTORY, OUTPUT_FILE_NAME, COLUMNS)
 
 
 if __name__ == "__main__":
